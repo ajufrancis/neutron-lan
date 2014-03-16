f2920ac656ace62313ecba7962aee0bf9f3056a1
alexanderplatz1999 <alexanderplatz1999@users.noreply.github.com>
2014-03-17 00:59:11 +0900

    update

--- ovsdb/ovsdb-client-debian.py
+++ ovsdb/ovsdb-client-debian.py
@@ -8,31 +8,63 @@ import socket, json
 s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 s.connect('/var/run/openvswitch/db.sock')
 
-query0 = {"method":"list_dbs", "params":[], "id": 0} 
-query1 =  {"method":"transact", "params":['Open_vSwitch',{"op":"select","table":"Open_vSwitch","where":[["_uuid","==",["uuid","d8149874-5bd9-4a58-88e8-106603b8a0e9"]]]}], "id": 0}
-query2 =  {"method":"transact", "params":['Open_vSwitch',{"op":"select","table":"Bridge","where":[["_uuid","==",["uuid","6f75d0a0-b2b8-489e-996b-d56172e02a5c"]]]}], "id": 0}
-query3 =  {"method":"transact", "params":['Open_vSwitch',{"op":"select","table":"Bridge","where":[["_uuid","==",["uuid","6f75d0a0-b2b8-489e-996b-d56172e02a5c"]]],"columns":["flow_entries"]}], "id": 0}
+id = 0
+uuid = ""
 
-print "method: list_dbs"
-s.send(json.dumps(query0))
-result0 = s.recv(4096)
-print result0
+seq = [
+{"method":"list_dbs", "params":[], "id": id}, 
+{"method":"transact", "params":['Open_vSwitch',{"op":"select","table":"NLAN","where":[]}], "id": id},
+{"method":"transact", 
+"params":[
+    "Open_vSwitch",
+    {
+        "op": "insert",
+        "table": "NLAN_Subnet",
+        "row":  {
+            "vid": 101,
+            "vni": 1001,
+            "ip_dvr": "10.0.0.1/24",
+            "ports": ["set", ["eth0", "veth-test"]]
+                },
+        "uuid-name": "vni1001"
+    },{
+        "op": "mutate",
+        "table": "NLAN",
+        "where": [],
+        "mutations": [
+            [
+                "subnets",
+                "insert",
+                [
+                    "set",
+                    [
+                        [
+                            "named-uuid",
+                            "vni1001"
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    }
+    ],
+"id": id},
+{"method":"transact", "params":['Open_vSwitch',{"op":"select","table":"Bridge","where":[["_uuid","==",["uuid", uuid]]],"columns":["flow_entries"]}], "id": id}
+]
+
+id = 0
+print "list_dbs"
+s.send(json.dumps(seq[id]))
+print s.recv(4096) 
 
 print ""
-print "method: transct, table: Open_vSwitch"
-s.send(json.dumps(query1))
-result1 = s.recv(4096)
-print result1
+id = 1
+print "transct select NLAN"
+s.send(json.dumps(seq[id]))
+print s.recv(4096) 
 
 print""
-print "method: transact, table: Bridge"
-s.send(json.dumps(query2))
-result2 = s.recv(4096)
-print result2
-
-print ""
-print "method: transact, table: Bridge, column: flow_entries"
-s.send(json.dumps(query3))
-result3 = s.recv(4096)
-print result3
-
+id = 2
+print "transact insert NLAN_Subnet"
+s.send(json.dumps(seq[id]))
+print s.recv(4096) 
