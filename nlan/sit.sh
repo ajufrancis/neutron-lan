2edc2aceed8a744704fd8752721954029e599ce6
alexanderplatz1999 <alexanderplatz1999@users.noreply.github.com>
2014-04-17 00:11:39 +0900

    pdf update

--- nlan/sit.sh
+++ nlan/sit.sh
@@ -5,12 +5,12 @@
 export PATH=$PATH:$(pwd)
 
 nlan_master.py --scpmod
-nlan_master.py init.run
+#nlan_master.py init.run
 #nlan_master.py init.run --info
-#nlan_master.py init.run --debug
-nlan_master.py dvsdvr.yaml
+nlan_master.py init.run --debug
+#nlan_master.py dvsdvr.yaml
 #nlan_master.py dvsdvr.yaml --info
-#nlan_master.py dvsdvr.yaml --debug
+nlan_master.py dvsdvr.yaml --debug
 
 ./vglobalip.sh
 ./ping.sh
