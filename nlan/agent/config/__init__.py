d6fe7d7dc35654cad185511a6890afcbfed7ca5d
alex <alexanderplatz1999@users.noreply.github.com>
2014-05-26 23:47:56 +0900

    Update __init__.py

--- nlan/agent/config/__init__.py
+++ nlan/agent/config/__init__.py
@@ -1,9 +1,9 @@
 # NLAN CONFIG Modules
 #
 # All the modules in this directory MUST support the following functions:
-# - add(model)
-# - update(model)
-# - delete(model)
+# - add()
+# - update()
+# - delete()
 #
 # "oputil" module provides utilities for state parameter manipulations
 # and OVSDB interractions.
